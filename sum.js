fcd670ff192448424bcc248bce6877bc38732868
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-04-17 23:39:59 -0500

    file cleanup

--- sum.js
+++
@@ -1,5 +0,0 @@
-const sum = (a, b) => a + b;
-
-const diff = (a, b) => a - b;
-
-module.exports = { sum, diff };
--- sum.test.js
+++
@@ -1,9 +0,0 @@
-const { sum, diff } = require('./sum');
-
-it('sums 3 + 2 to be 5', () => {
-  expect(sum(3, 2)).toBe(5);
-});
-
-it ('diffs 5 - 1 to 4', () => {
-  expect(diff(5, 1)).toBe(4);
-});
